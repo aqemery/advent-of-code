82b2b9310e914df720934255688aa2e1389d105e
Adam Emery <aqemery@gmail.com>
2019-12-02 09:47:28 -0500

    day2

---
+++ day2p1.py
@@ -0,0 +1,25 @@
+intcode = list(map(int, input().split(',')))
+
+def get_positions(pos):
+  return intcode[pos+1:pos+4]
+
+current_pos = 0
+opcode = intcode[current_pos]
+
+intcode[1] = 12
+intcode[2] = 2
+
+while opcode != 99:
+  if opcode == 1:
+    x,y,z = get_positions(current_pos)
+    intcode[z] = intcode[x] + intcode[y]
+  elif opcode == 2:
+    x,y,z = get_positions(current_pos)
+    intcode[z] = intcode[x] * intcode[y]
+  else:
+    print('opcode error')
+    break
+  current_pos += 4
+  opcode = intcode[current_pos]
+
+print(intcode)
---
+++ day2p2.py
@@ -0,0 +1,32 @@
+
+in_memory = list(map(int, input().split(',')))
+memory = list(in_memory)
+
+def get_params(pos):
+  return memory[pos+1:pos+4]
+
+for noun in range(0,99):
+  for verb in range(1,99):
+    memory = list(in_memory)
+    pointer = 0
+    opcode = memory[pointer]
+
+    memory[1] = noun
+    memory[2] = verb
+
+    while opcode != 99:
+      if opcode == 1:
+        x,y,z = get_params(pointer)
+        memory[z] = memory[x] + memory[y]
+      elif opcode == 2:
+        x,y,z = get_params(pointer)
+        memory[z] = memory[x] * memory[y]
+      else:
+        print('opcode error')
+        break
+      pointer += 4
+      opcode = memory[pointer]
+
+    if memory[0] == 19690720:
+      print(f'{noun:2.0f}{verb:2.0f}')
+      exit()
