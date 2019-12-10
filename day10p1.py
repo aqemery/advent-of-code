dab8793d750f02ad0cf57a12cf3ab5259dcd412b
Adam Emery <aqemery@gmail.com>
2019-12-10 13:47:12 -0500

    day10

---
+++ day10p1.py
@@ -0,0 +1,48 @@
+import itertools
+import math
+
+points = {}
+y = -1
+while True:
+  try:
+    y += 1
+    line = input()
+    for x in range(len(line)):
+      if line[x] == '#':
+        points[(x,y)] = 0
+  except EOFError:
+    break
+
+slight_lines = list(itertools.combinations(points.keys(),2))
+
+for sl in slight_lines:
+  x1,y1 = sl[0]
+  x2,y2 = sl[1]
+  dx = (x2 - x1)
+  dy = (y2 - y1)
+  div = math.gcd(dx,dy)
+  sx = dx // div
+  sy = dy // div
+  sight = True
+  tmp_x = x1 + sx
+  tmp_y = y1 + sy
+
+  while tmp_x != x2 or tmp_y != y2:
+    if (tmp_x,tmp_y) in points.keys():
+      sight = False
+      break
+    tmp_x = tmp_x + sx
+    tmp_y = tmp_y + sy
+  if sight:
+    for p in sl:
+      points[p] += 1
+
+max_value = 0
+max_key = None
+
+for k in points.keys():
+  if points[k] > max_value:
+    max_value = points[k]
+    max_key = k
+
+print(max_value, max_key)
---
+++ day10p2.py
@@ -0,0 +1,59 @@
+import itertools
+import math
+
+width = 0
+height = -1 
+points = []
+while True:
+  try:
+    height += 1
+    line = input()
+    width = len(line)
+    for x in range(width):
+      if line[x] == '#':
+        points.append((x,height))
+  except EOFError:
+    break
+
+station = (19, 14)
+points.remove(station)
+
+targets = {}
+for p in points:
+  dx = p[0]-station[0]
+  dy = p[1]-station[1]
+  dist = math.hypot(dx,dy)
+  angle = math.atan2(dx,dy)
+
+  if not angle in targets:
+    targets[angle] = []
+  targets[angle].append((p, dist))
+
+sk = list(targets.keys())
+sk.sort(reverse = True)
+
+index = 0
+count = 0
+last_destroyed = None
+while count < 200:
+  if index >= len(sk):
+    index = 0
+
+  angle_targets = targets[sk[index]]
+  least_at = angle_targets[0]
+  for at in angle_targets[1:]:
+    if at[1] < least_at[1]:
+      least_at = at
+  last_destroyed = least_at
+  angle_targets.remove(least_at)
+  count += 1
+  if len(angle_targets) == 0:
+    del targets[sk[index]]
+    del sk[index]
+  else:
+    index += 1
+
+ldp = last_destroyed[0]
+print(ldp[0]*100 + ldp[1])
+
+
