8edd30ecf1aeee38b5bb7210edf9f91f38a6610c
Adam Emery <aqemery@gmail.com>
2019-12-18 00:08:51 -0500

    day16p2

---
+++ day16p2.py
@@ -0,0 +1,9 @@
+fft = input()
+offset = int(fft[:7])
+fft = list(map(int, list(fft))) * 10000
+fft = fft[offset:]
+
+for loop in range(100):
+  for i in range(len(fft)-1, 0 ,-1):
+    fft[i-1] = int(str(fft[i-1] + fft[i])[-1:])
+print(''.join(map(str, fft[:8])))
