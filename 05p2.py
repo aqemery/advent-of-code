169bff8242938382137076d23829d6e829885188
Adam Emery <aqemery@gmail.com>
2020-12-05 10:33:59 -0500

    day 5 thunderlines

---
+++ 05p2.py
@@ -0,0 +1,15 @@
+import sys
+bi = {'B': '1', 'F': '0', 'L': '0', 'R': '1'}
+seats = [''.join(bi[c] for c in l) for l in sys.stdin.read().split()]
+ids = [int(s[:7], 2) * 8 + int(s[7:], 2) for s in seats] 
+ids.sort()
+last = ids[0]
+for i in ids[1:]:
+  if i > last + 1:
+    print(last + 1)
+  last = i
+
+
+
+
+
