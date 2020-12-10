54f3c4f8fd01232f3e615b48a3dee5e7dcf16c67
Adam Emery <aqemery@gmail.com>
2020-12-10 15:15:53 -0500

    take2

---
+++ 10p2-take2.py
@@ -0,0 +1,9 @@
+import sys
+
+adapters = [0] + list(map(int, sys.stdin.readlines()))
+adapters.sort(reverse=True)
+path = {adapters[0]:1}
+for a in adapters[1:]:
+  path[a] = sum([path[p] for p in range(a+1, a+4) if p in path])
+
+print(path[0])
