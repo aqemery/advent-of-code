5ecc636907bab6df106c9730fcb48cbf19b119f9
Adam Emery <aqemery@gmail.com>
2020-12-14 00:08:38 -0500

    Add files via upload

---
+++ 13p2-dumb.py
@@ -0,0 +1,13 @@
+import math
+input()
+
+buses = [(int(v),i) for i, v in enumerate(input().split(',')) if v != 'x']
+
+current = 0
+step = 1
+for value, offset in buses:
+    while (current + offset) % value != 0:
+        current += step
+    step *= value
+
+print(current)
