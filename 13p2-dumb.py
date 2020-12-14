ef5f239f7e9bc180eb095a5337282fddaa5b4d67
Adam Emery <aqemery@gmail.com>
2020-12-14 00:09:59 -0500

    Update 13p2-dumb.py

--- 13p2-dumb.py
+++ 13p2-dumb.py
@@ -6,8 +6,8 @@ buses = [(int(v),i) for i, v in enumerate(input().split(',')) if v != 'x']
 current = 0
 step = 1
 for value, offset in buses:
-    while (current + offset) % value != 0:
-        current += step
-    step *= value
+  while (current + offset) % value != 0:
+    current += step
+  step *= value
 
 print(current)
