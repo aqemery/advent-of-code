a8c947208ffb54b298bfc3acbc3fdc76d5075695
Adam Emery <aqemery@gmail.com>
2021-12-07 15:13:52 -0500

    readme

--- 2021/07p2.py
+++ 2021/07p2.py
@@ -1,5 +1,5 @@
 import math
 
 crabs = list(map(int,input().split(',')))
-average = math.floor(sum(crabs)/len(crabs))
+average = math.floor(sum(crabs)/len(crabs) + 0.5)
 print(sum([sum(range(1,abs(average-c)+1)) for c in crabs]))
