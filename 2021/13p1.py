48db0748b2fa7b5c0d163f2b217f80890185bf06
Adam Emery <aqemery@gmail.com>
2021-12-13 11:34:58 -0500

    update 13

--- 2021/13p1.py
+++ 2021/13p1.py
@@ -1,17 +1,14 @@
 import sys
-from collections import deque
 
 dots, folds = sys.stdin.read().split('\n\n')
 dots = set(tuple(int(c) for c in d.split(',')) for d in dots.split('\n'))
-folds = [f.split()[-1] for f in folds.split('\n')]
 
-for f in folds:
-  x, y = 0, 0
-  exec(f) 
-  if x:
-    dots = set((i-(i-x)*2, j) if i >= x else (i,j) for i, j in dots)
+for f in folds.split('\n'):
+  v = int(f.split('=')[-1])
+  if 'x' in f:
+    dots = set((i-(i-v)*2, j) if i >= v else (i,j) for i, j in dots)
   else:
-    dots = set((i, j-(j-y)*2) if j >= y else (i,j) for i, j in dots)
+    dots = set((i, j-(j-v)*2) if j >= v else (i,j) for i, j in dots)
   break
 
 print(len(dots))
--- 2021/13p2.py
+++ 2021/13p2.py
@@ -1,19 +1,16 @@
 import sys
-from collections import deque
 
 dots, folds = sys.stdin.read().split('\n\n')
 dots = set(tuple(int(c) for c in d.split(',')) for d in dots.split('\n'))
-folds = [f.split()[-1] for f in folds.split('\n')]
 
-for f in folds:
-  x, y = 0, 0
-  exec(f) 
-  if x:
-    dots = set((i-(i-x)*2, j) if i >= x else (i,j) for i, j in dots)
+for f in folds.split('\n'):
+  v = int(f.split('=')[-1])
+  if 'x' in f:
+    dots = set((i-(i-v)*2, j) if i >= v else (i,j) for i, j in dots)
   else:
-    dots = set((i, j-(j-y)*2) if j >= y else (i,j) for i, j in dots)
+    dots = set((i, j-(j-v)*2) if j >= v else (i,j) for i, j in dots)
 
-for y in range(10):
-  for x in range(40):
+for y in range(max(dots, key=lambda x:x[1])[1]+1):
+  for x in range(max(dots)[0]+1):
     print('*' if (x,y) in dots else ' ', end='')
   print()
