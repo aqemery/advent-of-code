8747e3a8638714fa8c54759c2d2b92bc3df2828e
Adam Emery <aqemery@gmail.com>
2021-12-14 14:32:14 -0500

    revert all counter

--- 2021/14p2.py
+++ 2021/14p2.py
@@ -1,16 +1,16 @@
 import sys
 from collections import defaultdict, Counter
-from functools import reduce
 import math
 
 polymer, pairs = sys.stdin.read().split('\n\n')
 rules = {a:[a[0]+b,b+a[1]] for a, b in [p.split(' -> ') for p in pairs.split('\n')]}
-
 counts = Counter(a+b for a, b in zip(polymer, polymer[1:]))
+
 for _ in range(40):
-  nc = Counter()
+  nc = defaultdict(int)
   for k, v in counts.items():
-    nc.update({p:v for  p in rules[k]})
+    for p in rules[k]:
+      nc[p] +=v
   counts = nc
 
 letter_counts = defaultdict(int)
