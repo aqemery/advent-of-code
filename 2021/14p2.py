6bef199cf992af5f4fb33bc92e44e799d81bba27
Adam Emery <aqemery@gmail.com>
2021-12-14 14:08:42 -0500

    update

--- 2021/14p2.py
+++ 2021/14p2.py
@@ -8,7 +8,10 @@ rules = {a:[a[0]+b,b+a[1]] for a, b in [p.split(' -> ') for p in pairs.split('\n
 
 counts = Counter(a+b for a, b in zip(polymer, polymer[1:]))
 for _ in range(40):
-  counts = reduce(lambda a, b: a + b, [Counter({p:v for  p in rules[k]}) for k, v in counts.items()])
+  nc = Counter()
+  for k, v in counts.items():
+    nc.update({p:v for  p in rules[k]})
+  counts = nc
 
 letter_counts = defaultdict(int)
 for k, v in counts.items():
