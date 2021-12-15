1753d96f9145f0baefb5beb18abc2361a0517281
Adam Emery <aqemery@gmail.com>
2021-12-15 12:59:32 -0500

    heapq

--- 2021/15p2.py
+++ 2021/15p2.py
@@ -1,14 +1,13 @@
 import sys
-from collections import deque
+import heapq
 
 lines = sys.stdin.read().split('\n')
 grid = {(x,y):int(v) for y, row in enumerate(lines) for x, v in enumerate(row)}
 around = [(1,0),(-1,0),(0,1),(0,-1)]
 
 start = (0,0)
+q = [(0,*start)]
 max_risk = {start:0}
-q = deque([start])
-
 big_grid = {}
 size = max(grid)[0] + 1
 
@@ -22,19 +21,20 @@ for x in range(5):
       big_grid[(ox+size*x,oy+size*y)] = new_risk
 grid = big_grid
 
+end = size*5 - 1
 while q:
-  pos = q.popleft()
-  adj = [tuple(sum(z) for z in zip(pos, a)) for a in around]
-  risk = max_risk.get(pos) 
+  risk, x, y = heapq.heappop(q)
+  adj = [(x+ax, y+ay) for ax, ay in around]
+  if x == y == end:
+    print(risk)
+    break
   for adj_pos in adj:
     if adj_risk := grid.get(adj_pos):
       next_risk = risk + adj_risk
       if current_risk := max_risk.get(adj_pos):
         if next_risk < current_risk:
-          q.append(adj_pos)
+          heapq.heappush(q, (next_risk, *adj_pos))
           max_risk[adj_pos] = next_risk
       else:
-        q.append(adj_pos)
+        heapq.heappush(q, (next_risk, *adj_pos))
         max_risk[adj_pos] = next_risk
-
-print(max_risk[max(max_risk)])
