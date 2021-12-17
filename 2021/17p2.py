4d1eed6ead7c5cedd34798521a6459648f04c29b
Adam Emery <aqemery@gmail.com>
2021-12-17 18:59:05 -0500

    cleanup but slower

--- 2021/17p2.py
+++ 2021/17p2.py
@@ -2,43 +2,26 @@ rx, ry = [map(int, dem.split('=')[1].split('..')) for dem in input().split(',')[
 lx, mx = rx
 ly, my = ry
 
-def step_x(vx):
-  x = 0
+def step(vx,vy):
+  x, y = 0, 0
   while vx > 0:
     x += vx
-    vx -= 1
-    yield x
-
-def step_both(vx,vy):
-  y = 0
-  endx = None
-  for x in step_x(vx):
     y += vy
+    vx -= 1
     vy -= 1
-    endx = x
     yield x, y
   while True:
     y += vy
     vy -= 1
-    yield endx, y
+    yield x, y
 
-def solve_cord(l,m):
-  vols = set()
-  for v in range(1,m+1):
-    for c in step_x(v):
-      if m >= c >= l:
-        vols.add(v)
-        break
-  return vols
-
-x_vols = solve_cord(lx, mx)
 found = set()
-for vx in x_vols:
-  for vy in range(ly, -ly+1):
-    for x,y in step_both(vx, vy):
+for vx in range(1,mx+1):
+  for vy in range(ly,-ly+1):
+    for x,y in step(vx, vy):
       if y < ly:
         break
-      if y <= my and y >= ly and mx >= x >= lx:
+      if ly <= y <= my and mx >= x >= lx:
         found.add((vx,vy))
 
 print(len(found))
