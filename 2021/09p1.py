4ac5f42dc7a338c7e59245e4959652bc8ada02ad
Adam Emery <aqemery@gmail.com>
2021-12-30 15:10:17 -0500

    9 fail

---
+++ 2021/09p1.py
@@ -0,0 +1,13 @@
+import sys
+
+grid = [[int(c) for c in l] for l in sys.stdin.read().split('\n')]
+around = [(1,0),(-1,0),(0,1),(0,-1)]
+
+def grid_check(v,x,y):
+  try:
+    return v < grid[y][x]
+  except IndexError:
+    return True
+
+low_points = [v for y, row in enumerate(grid) for x, v in enumerate(row) if all([grid_check(v,x+i,y+j) for i,j in around])]
+print(sum([1 + lp for lp in low_points]))
