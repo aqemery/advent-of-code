3500dc413bc857552e28d212204928ec09b7e02f
Adam Emery <aqemery@gmail.com>
2021-12-31 14:35:37 -0500

    update day 7

---
+++ 2021/07.py
@@ -0,0 +1,18 @@
+import math
+from statistics import median
+
+
+def part1(crabs):
+    med = int(median(crabs))
+    return sum([abs(med - c) for c in crabs])
+    
+
+def part2(crabs):
+    average = math.floor(sum(crabs) / len(crabs))
+    return sum([sum(range(1, abs(average - c) + 1)) for c in crabs])
+
+
+if __name__ == "__main__":
+    lines = list(map(int, input().split(",")))
+    print("part 1:", part1(lines))
+    print("part 2:", part2(lines))
--- 2021/07p1.py
+++
@@ -1,5 +0,0 @@
-from statistics import median
-
-crabs = list(map(int, input().split(",")))
-med = int(median(crabs))
-print(sum([abs(med - c) for c in crabs]))
