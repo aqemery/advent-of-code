03335faac00e986bef131705a5bdde1603992b89
Adam Emery <aqemery@gmail.com>
2021-12-31 14:13:07 -0500

    update template

--- template.py
+++ template.py
@@ -9,11 +9,7 @@ def part2(lines):
     return 
 
 
-def main():
+if __name__ == "__main__":
     lines = sys.stdin.readlines()
     print("part 1:", part1(lines))
     print("part 2:", part2(lines))
-
-
-if __name__ == "__main__":
-    main()
