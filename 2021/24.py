3a1d12ace870effddf2259a452e2f4c6d661fe14
Adam Emery <aqemery@gmail.com>
2021-12-31 15:01:52 -0500

    update 245

---
+++ 2021/24.py
@@ -0,0 +1,24 @@
+import sys
+
+def solve(ops, code):
+    stack = []
+    for i in range(14):
+        div, chk, add = map(int, [ops[i * 18 + x][-1] for x in [4, 5, 15]])
+        if div == 1:
+            stack.append((i, add))
+        elif div == 26:
+            j, add = stack.pop()
+            code[i] = code[j] + add + chk
+            if code[i] > 9:
+                code[j] -= code[i] - 9
+                code[i] = 9
+            if code[i] < 1:
+                code[j] += 1 - code[i]
+                code[i] = 1
+    return "".join(map(str, code))
+
+
+if __name__ == "__main__":
+    ops = [line.split() for line in sys.stdin.read().split("\n")]
+    print("part 1:", solve(ops, [9] * 14))
+    print("part 2:", solve(ops, [1] * 14))
