8ebccceedbd007d2967b6cad5892f4f092ac1ef0
Adam Emery <aqemery@gmail.com>
2021-12-31 14:03:08 -0500

    remove old day2

---
+++ template.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def part1(lines):
+    return
+
+
+def part2(lines):
+    return 
+
+
+def main():
+    lines = sys.stdin.readlines()
+    print("part 1:", part1(lines))
+    print("part 2:", part2(lines))
+
+
+if __name__ == "__main__":
+    main()
