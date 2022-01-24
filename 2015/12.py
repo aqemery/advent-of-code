5e36b85229dbfc38b8fd0cefec1b255949c59ff7
Adam Emery <aqemery@gmail.com>
2022-01-24 00:32:44 -0500

    2015-12

---
+++ 2015/12.py
@@ -0,0 +1,22 @@
+import re
+import json
+
+def step(data):
+    match data:
+        case list():
+            return sum(step(x) for x in data)
+        case dict():
+            vals = data.values()
+            if 'red' in vals:
+                return 0
+            return sum(step(x) for x in vals)
+        case int():
+            return data
+        case _:
+            return 0
+
+if __name__ == "__main__":
+    d = input()
+    print("part 1:", sum(int(x) for x in re.findall('-?\d+', d)))
+    d = json.loads(d)
+    print("part 2:", step(d))
