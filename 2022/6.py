4a0b4697c0b520195fe85fc4134c74044d398b3b
Adam Emery <aqemery@gmail.com>
2022-12-06 09:49:27 -0500

    day 6

---
+++ 2022/6.py
@@ -0,0 +1,12 @@
+import sys
+
+def solve(data, times):
+    for i in range(times, len(d)+1):
+        if len(set(d[i-times:i])) == times:
+            return i
+
+
+if __name__ == "__main__":
+    d = sys.stdin.read()
+    print("part 1:", solve(d, 4))
+    print("part 2:", solve(d, 14))
