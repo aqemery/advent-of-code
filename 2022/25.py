5e71adfd9eb6fcfc1e86b57020eb4032f2510e54
Adam Emery <aqemery@gmail.com>
2022-12-28 15:21:43 -0500

    2022 in the bag

--- 2022/25.py
+++ 2022/25.py
@@ -1,29 +1,48 @@
 import sys
-from collections import defaultdict, Counter, deque
-from itertools import combinations, permutations
-from math import prod
-from dataclasses import dataclass
-from functools import cache
-from statistics import median
-import re
-import heapq
-
-
-def solve(data, times):
-    return
-
-
-def part1(data):
-    return
-
-
-def part2(data):
-    return
-
 
 if __name__ == "__main__":
     d = sys.stdin.read().split("\n")
-    print("part 1:", part1(d))
-    print("part 2:", part2(d))
-    # print("part 1:", solve(d, 1))
-    # print("part 2:", solve(d, 2))
+    total = 0
+    for line in d:
+        number = ""
+        minus = ""
+        for c in line:
+            if c == "-":
+                number += "0"
+                minus += "1"
+            elif c == "=":
+                number += "0"
+                minus += "2"
+            else:
+                minus += "0"
+                number += c
+
+        total += int(number, 5) - int(minus, 5)
+
+    snafu = []
+    while total:
+        snafu.append(total % 5)
+        total //= 5
+
+    snafu.reverse()
+
+    while any(n > 2 for n in snafu):
+        next_snafu = []
+        for n in snafu:
+            if n <= 2:
+                next_snafu.append(n)
+            else:
+                next_snafu[-1] += 1
+                next_snafu.append(n - 5)
+        snafu = next_snafu
+    
+    out = ""
+    for n in snafu:
+        if n == -1:
+            out += "-"
+        elif n == -2:
+            out += "="
+        else:
+            out += str(n)
+
+    print("DONE:", out)
