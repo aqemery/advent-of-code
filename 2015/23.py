6584083bfefb17b95bc50d9d07291a5a7056b22b
Adam Emery <aqemery@gmail.com>
2023-10-16 14:56:06 -0400

    2015 day 23

---
+++ 2015/23.py
@@ -0,0 +1,48 @@
+import sys
+from collections import defaultdict, Counter, deque
+from itertools import combinations, permutations
+from math import prod
+from dataclasses import dataclass
+from functools import cache
+from statistics import median
+import re
+import heapq
+
+
+def solve(data, inital_a=0):
+    registers = {"a": inital_a, "b": 0}
+    instructions = [l.replace(",", "").split() for l in data]
+    i = 0
+
+    while i < len(instructions):
+        op = instructions[i]
+        match op:
+            case ["hlf", register]:
+                registers[register] //= 2
+            case ["tpl", register]:
+                registers[register] *= 3
+            case ["inc", register]:
+                registers[register] += 1
+            case ["jmp", offset]:
+                offset = int(offset)
+                i += offset
+                continue
+            case ["jie", register, offset]:
+                offset = int(offset)
+                if registers[register] % 2 == 0:
+                    i += offset
+                    continue
+            case ["jio", register, offset]:
+                offset = int(offset)
+                if registers[register] == 1:
+                    i += offset
+                    continue
+        i += 1
+
+    return registers["b"]
+
+
+if __name__ == "__main__":
+    d = sys.stdin.read().split("\n")
+    print("part 1:", solve(d))
+    print("part 2:", solve(d, inital_a=1))
