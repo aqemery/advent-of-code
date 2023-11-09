60f5439542d267a5b61ae7c97bdf3ff71f24bad7
Adam Emery <aqemery@gmail.com>
2023-11-08 19:59:49 -0500

    2018 day 1

--- 2018/1.py
+++ 2018/1.py
@@ -1,11 +1,24 @@
 import sys
 
-
-data = sys.stdin.read().split('\n')
-
-out = sum(int(n) for n in data)
-
-
-
-
+data = sys.stdin.read().split("\n")
+values = [int(n) for n in data]
+out = sum(values)
 print("part1:", out)
+
+totals = []
+
+current = 0
+for v in values:
+    current += v
+    totals.append(current)
+
+
+seen = set()
+current = 0
+while True:
+    for v in values:
+        current += v
+        if current in seen:
+            print("part2:", current)
+            exit()
+        seen.add(current)
