3d9e669af1f8a8fafb55971bb865ae0eccd798ec
Adam Emery <aqemery@gmail.com>
2023-12-04 09:57:11 -0500

    merge parts

--- 2023/4.py
+++ 2023/4.py
@@ -1,30 +1,18 @@
 import sys
 from collections import Counter
 
+data = sys.stdin.read().split("\n")
+total = 0
+more = Counter()
 
-def part1(data):
-    total = 0
-    for line in data:
-        a, b = [set(map(int, l.split())) for l in line.split(": ")[-1].split("|")]
-        if winners := len(a & b):
-            total += 2 ** (winners - 1)
-    return total
-
-
-def part2(data):
-    more = Counter()
-    for index, line in enumerate(data):
-        a, b = [set(map(int, l.split())) for l in line.split(": ")[-1].split("|")]
-        if winners := len(a & b):
-            more.update(
-                n
-                for _ in range(more[index] + 1)
-                for n in range(index + 1, index + winners + 1)
-            )
-    return sum(more.values()) + len(data)
-
-
-if __name__ == "__main__":
-    d = sys.stdin.read().split("\n")
-    print("part 1:", part1(d))
-    print("part 2:", part2(d))
+for index, line in enumerate(data):
+    a, b = [set(map(int, l.split())) for l in line.split(": ")[-1].split("|")]
+    if winners := len(a & b):
+        total += 2 ** (winners - 1)
+        more.update(
+            n
+            for _ in range(more[index] + 1)
+            for n in range(index + 1, index + winners + 1)
+        )
+print("part 1:", total)
+print("part 2:", sum(more.values()) + len(data))
