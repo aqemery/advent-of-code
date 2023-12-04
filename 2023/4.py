2543abe262ec0c865af8311bf04edf336e099b4b
Adam Emery <aqemery@gmail.com>
2023-12-04 10:13:24 -0500

    clean up

--- 2023/4.py
+++ 2023/4.py
@@ -9,10 +9,8 @@ for index, line in enumerate(data):
     a, b = [set(map(int, l.split())) for l in line.split(": ")[-1].split("|")]
     if winners := len(a & b):
         total += 2 ** (winners - 1)
-        more.update(
-            n
-            for _ in range(more[index] + 1)
-            for n in range(index + 1, index + winners + 1)
-        )
+        bonus = list(range(index + 1, index + winners + 1))
+        more.update(bonus * (more[index] + 1))
+
 print("part 1:", total)
 print("part 2:", sum(more.values()) + len(data))
