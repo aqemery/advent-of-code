b4257f4be841d2e57e228219555d0f23a986926d
Adam Emery <aqemery@gmail.com>
2023-12-08 14:34:34 -0500

    cycle

--- 2023/8.py
+++ 2023/8.py
@@ -1,18 +1,14 @@
 import sys
-from collections import deque
-from itertools import count
+from itertools import cycle
 from math import lcm
 
 
 def part1(start="AAA", end=lambda x: x == "ZZZ"):
     current = start
-    q = deque(dirs)
-    for times in count():
+    for times, dir in enumerate(cycle(dirs)):
         if end(current):
             return times
-        current = path[current][q[0]]
-        q.rotate(-1)
-
+        current = path[current][dir]
 
 def part2():
     current = set(k for k in path.keys() if k[-1] == "A")
