92118301a566bbac5b6da5684da91e3e89d3c448
Adam Emery <aqemery@gmail.com>
2023-12-09 10:43:00 -0500

    updated

--- 2023/9.py
+++ 2023/9.py
@@ -1,17 +1,17 @@
 import sys
 
 
-def solve(data, index=-1, fun=lambda a, b: b - a):
+def solve(data, reverse=1):
     all_numbers = []
     for line in data:
-        numbers = [[int(n) for n in line.split()]]
+        numbers = [[int(n) for n in line.split()][::reverse]]
         while any(numbers[-1]):
-            numbers.append([fun(a, b) for a, b in zip(numbers[-1], numbers[-1][1:])])
+            numbers.append([b-a for a, b in zip(numbers[-1], numbers[-1][1:])])
         all_numbers += numbers
-    return sum(n[index] for n in all_numbers)
+    return sum(n[-1] for n in all_numbers)
 
 
 if __name__ == "__main__":
     d = sys.stdin.read().split("\n")
     print("part 1:", solve(d))
-    print("part 2:", solve(d, index=0, fun=lambda a, b: a - b))
+    print("part 2:", solve(d, reverse=-1))
