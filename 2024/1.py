1741abb02da80afa4324691b00ab042c505daef9
Adam Emery <aqemery@gmail.com>
2024-12-01 21:25:58 -0500

    golf

--- 2024/1.py
+++ 2024/1.py
@@ -1,9 +1,6 @@
 import sys
-
 data = sys.stdin.read().split("\n")
-
 first = sorted(int(l.split("   ")[0]) for l in data)
 second = sorted(int(l.split("   ")[1]) for l in data)
-
 print("part 1:", sum(abs(a - b) for a, b in zip(first, second)))
 print("part 2:", sum(a * second.count(a) for a in first))
